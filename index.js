f64c38c3150e0268822bb0878ac93b4897662daf
Kassim Amir <amir.kassim946@gmail.com>
2025-07-09 20:23:15 +0300

    commit initial files of this project

---
+++ index.js
@@ -0,0 +1,59 @@
+const moviesSection = document.getElementById('movies')
+const searchArea = document.getElementById('search-area')
+const searchBtn = document.getElementById('search-btn')
+const addBtn = document.getElementById('add-btn')
+const addedMovieSect = document.getElementById('added-movie')
+let movie = ''
+let moviesArr = []
+
+searchBtn.addEventListener('click', function() {
+    if (searchArea.value == '') {
+        alert('Please, enter the name of the movie')
+    } else {
+        renderMovies()
+    }
+    addBtn.classList.remove('disabled')
+    searchArea.value = ''
+})
+
+
+function renderMovies () {
+    let movieHtml = ''
+    let searchedMovie = searchArea.value
+    fetch(`https://www.omdbapi.com/?apikey=40947cf7&t=${searchedMovie}`, {method:'GET'})
+    .then(res => res.json())
+    .then(data => {
+        movie = data
+        movieHtml = `
+            <img class="movie-poster" src="${movie.Poster}">
+            <div class="movie-details">
+                <div class="title-rating">
+                    <h2 class="title">${movie.Title}</h2>
+                    <p class="rating">${movie.imdbRating}</p>
+                </div>
+                <div class="time-type">
+                    <p>${movie.Runtime}</p>
+                    <p>${movie.Type}</p>
+                </div>
+                <p class="plot-story">${movie.Plot}</p>
+            </div>
+        `
+        moviesSection.insertAdjacentHTML("afterbegin", movieHtml)
+        })
+}
+
+addBtn.addEventListener('click', function() {
+    addMovie()
+})
+
+function addMovie () {
+       let addedMovie = {
+        poster: movie.Poster,
+        title:  movie.Title,
+        mins:   movie.Runtime,
+        type:   movie.Type
+    }
+    moviesArr.unshifts(addedMovie)
+    console.log(moviesArr)
+    console.log(moviesArr.length)
+}
